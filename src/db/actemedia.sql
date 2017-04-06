8f67d8f38833fecd41840f24a97bdb19332bebdc
davidmills <david.moulin@telecom-st-etienne.fr>
2017-04-06 10:35:11 +0200

    Ajout table client/imputation

--- src/db/actemedia.sql
+++ src/db/actemedia.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Client :  localhost:8889
--- Généré le :  Dim 19 Mars 2017 à 14:51
--- Version du serveur :  5.6.35
--- Version de PHP :  7.0.15
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -38,13 +31,13 @@ CREATE TABLE `projet` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `code` varchar(3) NOT NULL,
-  `client` varchar(255) NOT NULL,
+  `id_client` varchar(255) NOT NULL,
   `date_debut` date NOT NULL,
   `jours_vendus` int(11) NOT NULL,
   `jours_produits` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
+
 -- Contenu de la table `projet`
 --
 
@@ -52,6 +45,28 @@ INSERT INTO `projet` (`id`, `nom`, `code`, `client`, `date_debut`, `jours_vendus
 (1, 'Projet 1', 'TQ1', 'Client A', '2017-03-18', 88, 0),
 (2, 'Projet 2', 'REV', 'Client A', '2017-03-18', 0, 0),
 (3, 'Projet 3', 'REC', 'Client B', '2017-03-18', 0, 0);
+
+-- Structure de la table `imputation`
+--
+
+CREATE TABLE `imputation` (
+  `id` int(11) NOT NULL,
+  `code_projet` varchar(255) NOT NULL,
+  `code_collab` varchar(255) NOT NULL,
+  `jours` varchar(255) NOT NULL,
+  `valeur` date NOT NULL DEFAULT,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 --
 -- Index pour les tables exportées
