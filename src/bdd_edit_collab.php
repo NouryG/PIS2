dc7139b5ec7f9fde4a978dbc7404623212e001d0
David Moulin <david.moulin@telecom-st-etienne.fr>
2017-04-18 18:49:27 +0200

    Correction bug

--- src/bdd_edit_collab.php
+++ src/bdd_edit_collab.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=ACTEMEDIA;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=ACTEMEDIA;charset=utf8', 'root', 'root');
 }
 catch(Exception $e)
 {
