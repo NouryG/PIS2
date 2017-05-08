9049fd22b6ec08d808d63c84dec68f1f7c0f6318
RCTSE <RCharissou@gmail.com>
2017-05-08 22:55:20 +0200

    Modification de l'ajout des imputations
    Comme on crée toutes les imputations à 0 jours à la création d'un
    collaborateurs, quand on crée une imputation directement, elle met à
    jour celle qui existe déjà, ou alors la crée si elle n'existe pas (dans
    le cas où le projet est créé après la collaborateur, puisqu'à la
    création du collab on ne crée les imput que pour les projets existants à
    ce moment).

--- src/bdd_add_imput.php
+++ src/bdd_add_imput.php
@@ -8,17 +8,36 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-if( isset($_POST['code_projet']) && isset($_POST['code_collab']) && isset($_POST['jours'])){
+if (isset($_POST['code_projet']) && isset($_POST['code_collab']) && isset($_POST['jours'])) {
 
-	$req = $bdd->prepare('INSERT INTO imputation(code_projet, code_collab, jours) VALUES(:code_projet, :code_collab, :jours)');
-	$req->execute(array(
-    'code_projet' => $_POST['code_projet'],
-    'code_collab' => $_POST['code_collab'],
-    'jours' => $_POST['jours']
-    ));
+	// Vérification de l'existence de l'imputation, si oui on met à jour, si non on la crée
+    $check = $bdd->prepare('SELECT * FROM imputation WHERE code_projet LIKE :code_projet AND code_collab LIKE :code_collab');
+	$check->execute(array(
+	'code_projet' => $_POST['code_projet'],
+	'code_collab' => $_POST['code_collab'],
+	));
+
+    if ($check->rowCount() > 0) {
+        $req = $bdd->prepare('UPDATE imputation
+        					SET jours = :jours
+        					WHERE code_projet LIKE :code_projet AND code_collab LIKE :code_collab');
+        $req->execute(array(
+		'code_projet' => $_POST['code_projet'],
+		'code_collab' => $_POST['code_collab'],
+		'jours' => $_POST['jours']
+		));
     }
+    else {
+        $req = $bdd->prepare('INSERT INTO imputation(code_projet, code_collab, jours) VALUES(:code_projet, :code_collab, :jours)');
+		$req->execute(array(
+		'code_projet' => $_POST['code_projet'],
+		'code_collab' => $_POST['code_collab'],
+		'jours' => $_POST['jours']
+		));
+    }
+}
 
-else{
+else {
 	echo "L'ajout n'a pas abouti..";
 }
 ?>
