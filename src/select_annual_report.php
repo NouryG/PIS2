56e227f53c0b799afb4bb3118427b04fe0b21453
RCTSE <RCharissou@gmail.com>
2017-05-10 10:56:28 +0200

    totaux sans calcul

--- src/select_annual_report.php
+++ src/select_annual_report.php
@@ -39,6 +39,7 @@ while ($donnees = $reponse->fetch())
                <td>'.$donnees["code"].'</td>
                <td>'.$donnees["nom"].'</td>
                <td>'.$donnees["client"].'</td>
+               <td>'.$donnees["CA_vendu"].'</td>
                <td>'.$donnees["id"].'</td>
                <td>'.$donnees["id"].'</td>
                <td>'.$donnees["jours_vendus"].'</td>
@@ -47,19 +48,9 @@ while ($donnees = $reponse->fetch())
                <td>'.$donnees["RAF_reel"].'</td>
                <td>'.$donnees["id"].'</td>
                <td>'.$donnees["id"].'</td>
-               <td>'.$donnees["id"].'</td>
           </tr>
      ';
 }
-
-// Somme des jours produits
-$JP = $bdd->query('SELECT SUM(jours_produits) FROM projet');
-
-/*// Affichage du total jours produits
-$somme_JP = $JP->fetch();
-$output .= '
-    <td>'.$somme_JP["total_JP"].'</td>
-';*/
 
 // Affichage des totaux
 $output .= '
@@ -67,15 +58,69 @@ $output .= '
         <th>Totaux</th>
         <th></th>
         <th></th>
-        <th>Total</th>
-        <th>Total</th>
-        <th>Total</th>
-        <th>Total</th>
-        <th></th>
-        <th>Total</th>
-        <th>Total</th>
-        <th></th>
-        <th>Total</th>
+';
+
+// Somme des commandes
+$CA_vendu = $bdd->query('SELECT SUM(CA_vendu) AS somme_CA_vendu FROM projet');
+
+// Affichage du total commande
+$somme_CA = $CA_vendu->fetch();
+$output .= '
+    <th>'.$somme_CA["somme_CA_vendu"].'</th>
+    <th></th>
+    <th></th>
+';
+
+/*// Somme du produit AMA
+$cmd = $bdd->query('SELECT SUM(cout_projet) AS somme_cout FROM projet');
+
+// Affichage du total produit AMA
+$somme_commande = $cmd->fetch();
+$output .= '
+    <th>'.$somme_commande["somme_cout"].'</th>
+';*/
+
+/*// Somme du produit STT
+$cmd = $bdd->query('SELECT SUM(cout_projet) AS somme_cout FROM projet');
+
+// Affichage du total produit STT
+$somme_commande = $cmd->fetch();
+$output .= '
+    <th>'.$somme_commande["somme_cout"].'</th>
+';*/
+
+// Somme des jours vendus
+$JV = $bdd->query('SELECT SUM(jours_vendus) AS somme_JV FROM projet');
+
+// Affichage du total jours vendus
+$somme_JV = $JV->fetch();
+$output .= '
+    <th>'.$somme_JV["somme_JV"].'</th>
+    <th></th>
+';
+
+// Somme des jours produits
+$JP = $bdd->query('SELECT SUM(jours_produits) AS somme_JP FROM projet');
+
+// Affichage du total jours produits
+$somme_JP = $JP->fetch();
+$output .= '
+    <th>'.$somme_JP["somme_JP"].'</th>
+';
+
+// Somme des RAF
+$RAF = $bdd->query('SELECT SUM(RAF_reel) AS somme_RAF FROM projet');
+
+// Affichage du total jours produits
+$somme_RAF = $RAF->fetch();
+$output .= '
+    <th>'.$somme_RAF["somme_RAF"].'</th>
+    <th></th>
+    <th></th>
+';
+
+// Fin de la ligne des totaux
+$output .= '
     </tr>
 ';
 
