293b9d47032e910d5838098136f7b28c141e0d22
RCTSE <RCharissou@gmail.com>
2017-05-16 17:58:24 +0200

    hotfix title

--- src/monthly_report.php
+++ src/monthly_report.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Table collaborateurs</title>
+        <title>Rapport mensuel</title>
         <link rel="icon" type="image/png" href="assets/ico/ms-icon-150x150.png">
 
         <!-- CSS -->
