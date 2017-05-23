5ad87745b4983abef1d965cd47645ed798451f26
RCTSE <RCharissou@gmail.com>
2017-05-24 00:56:24 +0200

    solve db voids

--- src/db/actemedia.sql
+++ src/db/actemedia.sql
@@ -1,26 +1,32 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Mar 16 Mai 2017 à 18:35
--- Version du serveur :  5.6.35
--- Version de PHP :  7.0.15
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 23, 2017 at 10:55 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de données :  `ACTEMEDIA`
+-- Database: `actemedia`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `collaborateurs`
+-- Table structure for table `collaborateurs`
 --
 
-CREATE TABLE `collaborateurs` (
+CREATE TABLE IF NOT EXISTS `collaborateurs` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
@@ -28,51 +34,47 @@ CREATE TABLE `collaborateurs` (
   `societe` varchar(3) NOT NULL,
   `TJ` int(11) NOT NULL DEFAULT '0',
   `actif` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `collaborateurs`
+-- Dumping data for table `collaborateurs`
 --
 
 INSERT INTO `collaborateurs` (`id`, `nom`, `prenom`, `code`, `societe`, `TJ`, `actif`) VALUES
-(4, 'Noury', 'Ghali', 'NRY', 'AMA', 12232, 1),
-(5, 'Charissouu', 'Romain', 'RAC', 'AMA', 2333, 1),
-(6, 'Gosset', 'Cedric', 'GOS', 'EME', 1233, 1),
-(7, 'Khalil', 'Omar', 'KHL', 'TEC', 2211, 1),
-(8, 'Charkaoui', 'Othmane', 'CHA', 'AMA', 1233, 1);
+(4, 'Noury', 'Ghali', 'NRY', 'AMA', 1200, 1),
+(9, 'Charissou', 'Romain', 'RCA', 'EXT', 2000, 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `imputation`
+-- Table structure for table `imputation`
 --
 
-CREATE TABLE `imputation` (
+CREATE TABLE IF NOT EXISTS `imputation` (
   `id` int(11) NOT NULL,
   `code_projet` varchar(255) NOT NULL,
   `code_collab` varchar(255) NOT NULL,
   `jours` varchar(255) NOT NULL,
   `date_imput` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `imputation`
+-- Dumping data for table `imputation`
 --
 
 INSERT INTO `imputation` (`id`, `code_projet`, `code_collab`, `jours`, `date_imput`) VALUES
 (1, 'FAL', 'NRY', '21', '2017-07-01'),
-(2, 'FIF', 'NRY', '2', '2017-05-01'),
-(5, 'MAC', 'KHL', '2', '2017-02-01'),
-(6, 'FIF', 'GOS', '3', '2017-05-01'),
-(7, 'MAC', 'NRY', '12', '2017-07-01');
+(7, 'MAC', 'NRY', '12', '2017-07-01'),
+(8, 'FAL', 'RCA', '0', '2017-03-01'),
+(9, 'MAC', 'RCA', '0', '2017-03-01');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `projet`
+-- Table structure for table `projet`
 --
 
-CREATE TABLE `projet` (
+CREATE TABLE IF NOT EXISTS `projet` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `code` varchar(3) NOT NULL,
@@ -83,55 +85,57 @@ CREATE TABLE `projet` (
   `CA_vendu` float DEFAULT '0',
   `cout_projet` float DEFAULT '0',
   `RAF_reel` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `projet`
+-- Dumping data for table `projet`
 --
 
 INSERT INTO `projet` (`id`, `nom`, `code`, `client`, `date_debut`, `jours_vendus`, `jours_produits`, `CA_vendu`, `cout_projet`, `RAF_reel`) VALUES
 (1, 'Fallout', 'FAL', 'Bethesda', '2017-03-29', 25, 0, 0, 0, 2),
-(2, 'Macbook', 'MAC', 'Apple', '2017-03-30', 25, 0, 0, 0, 12),
-(4, 'FIFA', 'FIF', 'EA', '2017-05-11', 45, 0, 100000, 0, 44);
+(2, 'Macbook', 'MAC', 'Apple', '2017-03-30', 25, 0, 0, 0, 12);
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `collaborateurs`
+-- Indexes for table `collaborateurs`
 --
 ALTER TABLE `collaborateurs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `imputation`
+-- Indexes for table `imputation`
 --
 ALTER TABLE `imputation`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `projet`
+-- Indexes for table `projet`
 --
 ALTER TABLE `projet`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `collaborateurs`
+-- AUTO_INCREMENT for table `collaborateurs`
 --
 ALTER TABLE `collaborateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT pour la table `imputation`
+-- AUTO_INCREMENT for table `imputation`
 --
 ALTER TABLE `imputation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
--- AUTO_INCREMENT pour la table `projet`
+-- AUTO_INCREMENT for table `projet`
 --
 ALTER TABLE `projet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
