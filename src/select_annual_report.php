374721a0a3d8dd7ddcece026804cac3a6eeea974
RCTSE <RCharissou@gmail.com>
2017-05-24 00:44:14 +0200

    un peu mieux sur la largeur

--- src/select_annual_report.php
+++ src/select_annual_report.php
@@ -12,8 +12,7 @@ catch(Exception $e)
 
 $reponse = $bdd->query('SELECT * FROM projet');
 $output = '
-<div class="col-sm-10 col-sm-offset-1"
-    <div class="table-responsive">
+    <div class="table-responsive" style="width: 90%; margin: auto;">
         <table class="table table-bordered">
             <thead>
                 <tr class="bg-primary" style="background-color: #2F93DB;">
@@ -251,7 +250,7 @@ $output .= '
 
  $output .= '</table>
       </div>
-    </div>';
+ ';
 
  echo $output;
  ?>
