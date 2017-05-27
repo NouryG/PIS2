6bdf683ad510a1ed015db9a1cf0aec812ffb4ab7
NouryG <michaelfr33man@gmail.com>
2017-05-27 12:33:04 +0200

    BDD de TEST FINALE

--- src/db/actemedia.sql
+++ src/db/actemedia.sql
@@ -1,32 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 23, 2017 at 10:55 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.0.13
+-- Client :  localhost:8889
+-- Généré le :  Sam 27 Mai 2017 à 12:32
+-- Version du serveur :  5.6.35
+-- Version de PHP :  7.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `actemedia`
+-- Base de données :  `ACTEMEDIA`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `collaborateurs`
+-- Structure de la table `collaborateurs`
 --
 
-CREATE TABLE IF NOT EXISTS `collaborateurs` (
+CREATE TABLE `collaborateurs` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
@@ -34,47 +28,66 @@ CREATE TABLE IF NOT EXISTS `collaborateurs` (
   `societe` varchar(3) NOT NULL,
   `TJ` int(11) NOT NULL DEFAULT '0',
   `actif` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `collaborateurs`
+-- Contenu de la table `collaborateurs`
 --
 
 INSERT INTO `collaborateurs` (`id`, `nom`, `prenom`, `code`, `societe`, `TJ`, `actif`) VALUES
-(4, 'Noury', 'Ghali', 'NRY', 'AMA', 1200, 1),
-(9, 'Charissou', 'Romain', 'RCA', 'EXT', 2000, 1);
+(1, 'Noury', 'Ghali', 'NRY', 'AMA', 500, 1),
+(2, 'Moulin', 'David', 'MOU', 'CAP', 700, 1),
+(3, 'Garrin', 'Pierre', 'GAR', 'AMA', 450, 1),
+(4, 'Charissou', 'Romain', 'CHR', 'IBM', 500, 1),
+(5, 'Gosset', 'Cedric', 'GOS', 'AMA', 300, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `imputation`
+-- Structure de la table `imputation`
 --
 
-CREATE TABLE IF NOT EXISTS `imputation` (
+CREATE TABLE `imputation` (
   `id` int(11) NOT NULL,
   `code_projet` varchar(255) NOT NULL,
   `code_collab` varchar(255) NOT NULL,
   `jours` varchar(255) NOT NULL,
   `date_imput` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `imputation`
+-- Contenu de la table `imputation`
 --
 
 INSERT INTO `imputation` (`id`, `code_projet`, `code_collab`, `jours`, `date_imput`) VALUES
-(1, 'FAL', 'NRY', '21', '2017-07-01'),
-(7, 'MAC', 'NRY', '12', '2017-07-01'),
-(8, 'FAL', 'RCA', '0', '2017-03-01'),
-(9, 'MAC', 'RCA', '0', '2017-03-01');
+(1, 'FIF', 'NRY', '10', '2017-05-01'),
+(2, 'FIF', 'MOU', '0', '2017-05-01'),
+(3, 'FIF', 'GAR', '0', '2017-05-01'),
+(4, 'FIF', 'CHR', '15', '2017-05-01'),
+(5, 'FIF', 'GOS', '5', '2017-05-01'),
+(6, 'GTA', 'NRY', '0', '2017-05-01'),
+(7, 'GTA', 'MOU', '5', '2017-05-01'),
+(8, 'GTA', 'GAR', '10', '2017-05-01'),
+(9, 'GTA', 'CHR', '5', '2017-05-01'),
+(10, 'GTA', 'GOS', '0', '2017-05-01'),
+(11, 'FIF', 'NRY', '15', '2017-06-01'),
+(12, 'GTA', 'NRY', '0', '2017-06-01'),
+(13, 'FIF', 'GOS', '5', '2017-06-01'),
+(14, 'GTA', 'GOS', '0', '2017-06-01'),
+(15, 'FIF', 'CHR', '10', '2017-06-01'),
+(16, 'GTA', 'CHR', '5', '2017-06-01'),
+(17, 'GTA', 'MOU', '10', '2017-06-01'),
+(18, 'FIF', 'MOU', '0', '2017-06-01'),
+(19, 'GTA', 'GAR', '15', '2017-06-01'),
+(20, 'FIF', 'GAR', '0', '2017-06-01');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projet`
+-- Structure de la table `projet`
 --
 
-CREATE TABLE IF NOT EXISTS `projet` (
+CREATE TABLE `projet` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `code` varchar(3) NOT NULL,
@@ -85,57 +98,54 @@ CREATE TABLE IF NOT EXISTS `projet` (
   `CA_vendu` float DEFAULT '0',
   `cout_projet` float DEFAULT '0',
   `RAF_reel` int(11) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `projet`
+-- Contenu de la table `projet`
 --
 
 INSERT INTO `projet` (`id`, `nom`, `code`, `client`, `date_debut`, `jours_vendus`, `jours_produits`, `CA_vendu`, `cout_projet`, `RAF_reel`) VALUES
-(1, 'Fallout', 'FAL', 'Bethesda', '2017-03-29', 25, 0, 0, 0, 2),
-(2, 'Macbook', 'MAC', 'Apple', '2017-03-30', 25, 0, 0, 0, 12);
+(1, 'FIFA', 'FIF', 'EASPORTS', '2017-05-25', 100, 0, 50000, 0, 0),
+(2, 'Grand Theft Auto', 'GTA', 'ROCKSTAR', '2017-05-27', 150, 0, 80000, 0, 0);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `collaborateurs`
+-- Index pour la table `collaborateurs`
 --
 ALTER TABLE `collaborateurs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `imputation`
+-- Index pour la table `imputation`
 --
 ALTER TABLE `imputation`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `projet`
+-- Index pour la table `projet`
 --
 ALTER TABLE `projet`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `collaborateurs`
+-- AUTO_INCREMENT pour la table `collaborateurs`
 --
 ALTER TABLE `collaborateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `imputation`
+-- AUTO_INCREMENT pour la table `imputation`
 --
 ALTER TABLE `imputation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
--- AUTO_INCREMENT for table `projet`
+-- AUTO_INCREMENT pour la table `projet`
 --
 ALTER TABLE `projet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
